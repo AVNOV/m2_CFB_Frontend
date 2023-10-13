2a8204ca35d4a563a85b5bd46240865dd1d37988
Edouard <edouard.laffont@live.com>
2023-10-13 13:50:24 +0200

    feat - axios query and auth for user

---
+++ api/API.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const baseURL = `https://localhost:5555/api/`;
+
+const API = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+API.interceptors.request.use(
+  async (config) => {
+    const access_token = localStorage.getItem('access_token');
+    if (access_token) {
+      config.headers['Authorization'] = `Bearer ${access_token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default API;
