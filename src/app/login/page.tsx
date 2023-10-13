14e00ad963b354f82c895c51e9fc801872464edb
Corentin Faget <faget.corentin@gmail.com>
2023-10-13 11:03:31 +0200

    new button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,9 @@
+import Button from '../components/Button';
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="h-[100vh] w-[100vw] bg-white">
+      <Button>Créer une partie</Button>
+    </div>
+  );
 }
