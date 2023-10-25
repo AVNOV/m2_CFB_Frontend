d1c100cd931c63c040e809647c11b40af57dcef0
Edouard <edouard.laffont@live.com>
2023-10-25 14:36:02 +0200

    fix - refresh connexion

--- src/app/components/Reload/index.tsx
+++ src/app/components/Reload/index.tsx
@@ -2,16 +2,17 @@ import { useEffect } from 'react';
 import { useAppDispatch } from '../../../../store';
 import { login } from '../../../../slices/auth.slice';
 
-export default function Reload() {
+export default function Refresh() {
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    const user = localStorage.getItem('user');
-    const accessToken = localStorage.getItem('access_token');
 
-    if (user && accessToken) {
-      dispatch(login({ user: JSON.parse(user), accessToken: accessToken }));
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    const access_token = localStorage.getItem('access_token')!;
+    if (user && access_token) {
+      dispatch(login({ user: user, access_token: access_token }));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return <></>;
 }
