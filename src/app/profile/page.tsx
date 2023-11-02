dfd5ba05af70bf5ee6f14222a510a27701a73e01
Edouard <edouard.laffont@live.com>
2023-11-02 09:47:08 +0100

    feat - profile page display user data

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Image from 'next/image';
+import { useAppSelector } from '../../../store';
+import arrow from '@/assets/icons/arrow.svg';
+import { useRouter } from 'next/navigation';
+
+export default function Page() {
+  const router = useRouter();
+  const user = useAppSelector((state) => state.auth.user);
+
+  return (
+    <div className="h-full w-full flex flex-col pt-5">
+      <div className="flex cursor-pointer" onClick={() => router.push('/')}>
+        <Image
+          className="w-4 object-contain ml-2 rotate-90"
+          src={arrow}
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col my-auto justify-center items-center">
+        <h1 className="text-5xl mb-10">Mes informations</h1>
+        <div className="flex flex-col items-center space-y-4 w-1/3">
+          <p>
+            {user.firstname} {user.lastname}
+          </p>
+          <p>{user.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
