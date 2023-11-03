8a20db90d0502215395c03565af387715b9df169
Corentin Faget <faget.corentin@gmail.com>
2023-11-03 08:50:16 +0100

    test: add input testing

--- src/app/components/InputText.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import InputText from './InputText';
-
-describe('<InputText />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<InputText inputType="text" />);
-  });
-});
