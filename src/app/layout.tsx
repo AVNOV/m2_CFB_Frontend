235ed1d5aea8fc941e40d5f1e64d8dd9413a5352
Glenn <oberle.glenn@icloud.com>
2023-11-03 11:05:22 +0100

    Add easter egg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,9 @@ import store from '../../store';
 import Reload from './components/Reload';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
+import { useState } from 'react';
+import Modal from './components/Modal';
 
 const inter = Lilita_One({ subsets: ['latin'], weight: '400' });
 
@@ -20,15 +22,56 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [modal, setModal] = useState(false);
+  console.log(modal);
+
+  useEffect(() => {
+    let keyPressed = false;
+    const keyDown = (event: KeyboardEvent) => {
+      if (event.ctrlKey) keyPressed = true;
+    };
+
+    const keyUp = (event: KeyboardEvent) => {
+      if (event.key === 'Control') {
+        keyPressed = false;
+        setModal(false);
+      }
+    };
+
+    const mouseMouve = (event: MouseEvent) => {
+      if (keyPressed) {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        const windowWidth = window.innerWidth;
+
+        const margin = 50;
+        if (mouseX >= windowWidth - margin && mouseY <= margin) setModal(true);
+        else setModal(false);
+      }
+    };
+
+    window.addEventListener('keydown', (event) => keyDown(event));
+    window.addEventListener('keyup', (event) => keyUp(event));
+    window.addEventListener('mousemove', (event) => mouseMouve(event));
+
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+      window.removeEventListener('keyup', keyUp);
+      window.removeEventListener('mousemove', mouseMouve);
+    };
+  }, []);
+
   return (
     <QueryClientProvider client={client}>
       <Provider store={store}>
         <ToastContext.Provider value={{ toast: toast }}>
           <Reload />
-          <html lang="en">
+          <html lang="fr">
             <body className={inter.className}>
               {children}
               <ToastContainer position="bottom-right" theme="colored" />
+              {modal && <Modal />}
             </body>
           </html>
         </ToastContext.Provider>
