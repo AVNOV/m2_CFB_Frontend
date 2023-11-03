a9cd181c65fe775fa37f3977a0e32b001b8d918e
Edouard <edouard.laffont@live.com>
2023-11-03 10:12:50 +0100

    feat - get user with dto

--- api/query/user.query.ts
+++ api/query/user.query.ts
@@ -1,5 +1,6 @@
 import API from '../API';
 import { CreateUserType } from '../../types/User/CreateUserType';
+import { UserType } from 'types/User/UserType';
 
 export const loginRequest = async (email: string, password: string) => {
   const { data } = await API.post('/login', { email, password });
@@ -13,5 +14,10 @@ export const loginRequest = async (email: string, password: string) => {
 
 export const createUser = async (user: CreateUserType) => {
   const { data } = await API.post('/register', user);
+  return data;
+};
+
+export const getUser = async () => {
+  const data: UserType = await API.get('/user');
   return data;
 };
