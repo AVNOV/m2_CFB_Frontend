e94f560a472100bd997db401d6512443342a338c
Edouard <edouard.laffont@live.com>
2023-11-03 09:39:22 +0100

    feat - quizz dto

---
+++ types/Quizz/QuizzType.ts
@@ -0,0 +1,12 @@
+import { UserType } from 'types/User/UserType';
+
+export type QuizzType = {
+  id: number;
+  title: string;
+  difficulty: number;
+  themeId: number;
+  userId: number | null;
+  user: UserType | null;
+  questions: QuestionType[];
+  games: GameType[];
+};
--- types/User/UserType.ts
+++ types/User/UserType.ts
@@ -1,4 +1,4 @@
-export type CreateUserType = {
+export type UserType = {
   firstname: string;
   lastname: string;
   email: string;
