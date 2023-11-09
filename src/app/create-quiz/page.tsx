f2e5f28540ba0e241fdda6730ca114849e8500aa
Edouard <edouard.laffont@live.com>
2023-11-09 15:35:59 +0100

    feat - page create quiz template ok

--- src/app/create-quiz/page.tsx
+++ src/app/create-quiz/page.tsx
@@ -1,95 +1,129 @@
 'use client';
-
 import { useState } from 'react';
 
+import { getThemes } from 'api/query/theme.query';
+import { ThemeType } from 'types/ThemeTypes';
+import { QuizType } from 'types/QuizTypes';
+import { QuestionType } from 'types/QuestionTypes';
+import Button from '../components/Button';
+
 export default function Page() {
-  const [quizName, setQuizName] = useState('');
-  const [quizTheme, setQuizTheme] = useState('');
-  const [questions, setQuestions] = useState(
-    Array(10).fill({
-      question: '',
-      answers: ['', '', '', ''],
-      correctAnswer: 0,
-    }),
-  );
+  const [themes, setThemes] = useState<ThemeType[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-  const handleQuestionChange = (index, value) => {
-    const updatedQuestions = [...questions];
-    updatedQuestions[index].question = value;
-    setQuestions(updatedQuestions);
+  const [quizData, setQuizData] = useState<QuizType>({
+    id: 0,
+    title: '',
+    difficulty: 0,
+    themeId: 0,
+    theme: { id: 0, name: '' },
+    userId: null,
+    user: null,
+    questions: [],
+    games: [],
+  });
+
+  const fetchThemesData = async () => {
+    try {
+      const themesData = await getThemes();
+      setThemes(themesData);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des thèmes', error);
+    }
   };
 
-  const handleAnswerChange = (questionIndex, answerIndex, value) => {
-    const updatedQuestions = [...questions];
-    updatedQuestions[questionIndex].answers[answerIndex] = value;
-    setQuestions(updatedQuestions);
+  if (themes.length === 0) {
+    fetchThemesData();
+  }
+
+  const handleQuizTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuizData({ ...quizData, title: e.target.value });
   };
 
-  const handleCorrectAnswerChange = (questionIndex, value) => {
-    const updatedQuestions = [...questions];
-    updatedQuestions[questionIndex].correctAnswer = parseInt(value, 10);
-    setQuestions(updatedQuestions);
+  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const themeId = parseInt(e.target.value, 10);
+    setQuizData({ ...quizData, themeId });
+  };
+
+  const addQuestion = () => {
+    const newQuestion: QuestionType = {
+      id: quizData.questions.length + 1,
+      title: '',
+      quizzes: [quizData],
+      answers: [],
+    };
+    setQuizData({
+      ...quizData,
+      questions: [...quizData.questions, newQuestion],
+    });
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
   };
 
   return (
-    <div className="flex flex-col w-full h-full pt-10 items-center">
+    <div className="flex flex-col p-4 w-full h-full pt-10 items-center">
       <p className="text-4xl mb-8">Créez votre quiz !</p>
-      <div className="flex flex-col w-full h-full items-start">
-        <div className="mb-4">
-          <label>Nom du quiz:</label>
-          <input
-            type="text"
-            value={quizName}
-            onChange={(e) => setQuizName(e.target.value)}
-          />
-        </div>
 
-        <div className="mb-4">
-          <label>Thème du quiz:</label>
-          <input
-            type="text"
-            value={quizTheme}
-            onChange={(e) => setQuizTheme(e.target.value)}
-          />
-        </div>
+      <label htmlFor="quizTitle" className="mb-2">
+        Titre du quiz:
+      </label>
+      <input
+        type="text"
+        id="quizTitle"
+        value={quizData.title}
+        onChange={handleQuizTitleChange}
+        className="border-2 focus:outline-orange-400 border-grey rounded-lg py-2 px-3 leading-tight border-solid bg-white text-black"
+      />
 
-        <div className="flex flex-col">
-          <p>Questions:</p>
-          {questions.map((q, index) => (
-            <div key={index} className="mb-4">
-              <p>Question {index + 1}:</p>
+      <label htmlFor="theme" className="mt-5 mb-2">
+        Thème:
+      </label>
+      <select
+        id="theme"
+        value={quizData.themeId}
+        onChange={handleThemeChange}
+        className="border-2 focus:outline-orange-400 border-grey rounded-lg py-2 px-3 leading-tight border-solid bg-white text-black"
+      >
+        {themes.map((theme) => (
+          <option key={theme.id} value={theme.id}>
+            <p className="text-black">{theme.name}</p>
+          </option>
+        ))}
+      </select>
+
+      <div className="w-full h-full flex flex-col items-center">
+        <p className="mt-10 mb-2">Questions {currentQuestionIndex + 1}:</p>
+
+        <input
+          type="text"
+          className="border-2 focus:outline-orange-400 border-grey rounded-lg py-2 px-3 leading-tight border-solid bg-white text-black w-1/4"
+        />
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <p className="mt-4 mb-2">Réponse juste:</p>
+
+            <input
+              type="text"
+              className="border-2 focus:outline-orange-400 border-grey rounded-lg py-2 px-3 leading-tight border-solid bg-white text-black"
+            />
+          </div>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <div key={index} className="flex flex-col">
+              <p className={`mt-4 ml-4 mb-2`}>Réponse fausse {index + 1}:</p>
               <input
                 type="text"
-                value={q.question}
-                onChange={(e) => handleQuestionChange(index, e.target.value)}
+                className="border-2 focus:outline-orange-400 border-grey rounded-lg py-2 px-3 leading-tight border-solid bg-white text-black ml-4"
               />
-
-              <p>Réponses:</p>
-              {q.answers.map((answer, answerIndex) => (
-                <div key={answerIndex}>
-                  <input
-                    className="mb-2"
-                    type="text"
-                    value={answer}
-                    onChange={(e) =>
-                      handleAnswerChange(index, answerIndex, e.target.value)
-                    }
-                  />
-                  <label>
-                    <input
-                      type="radio"
-                      value={answerIndex}
-                      checked={q.correctAnswer === answerIndex}
-                      onChange={() =>
-                        handleCorrectAnswerChange(index, answerIndex)
-                      }
-                    />
-                    Correct
-                  </label>
-                </div>
-              ))}
             </div>
           ))}
+        </div>
+        <div className="my-10">{`${currentQuestionIndex + 1}/10`}</div>
+        <div className="w-1/2 flex flex-col items-center">
+          {currentQuestionIndex < 9 && (
+            <Button className="" onClick={addQuestion}>
+              Ajouter la question suivante
+            </Button>
+          )}
+          <Button className="mt-5">Valider le quiz</Button>
         </div>
       </div>
     </div>
