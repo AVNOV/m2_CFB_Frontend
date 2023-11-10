0ca0f7b1ce54935ff8738d60e2e4526ffb801944
Corentin Faget <faget.corentin@gmail.com>
2023-11-10 15:04:17 +0100

    fix: fix button click

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -8,21 +8,21 @@ type props = {
 
 export default function Button(props: props) {
   return (
-    <div
-      className={` px-2 py-1 w-fit rounded-md cursor-pointer transition-transform active:scale-95 ${
+    <button
+      disabled={props.disabled}
+      type={props.type}
+      onClick={props.disabled ? () => {} : props.onClick}
+      className={` px-2 py-2 w-fit rounded-md cursor-pointer transition-transform active:scale-95 ${
         props.disabled ? 'bg-gray-400' : 'bg-orange-400'
       } ${props.className}`}
     >
-      <button
-        disabled={props.disabled}
-        type={props.type}
-        onClick={props.disabled ? () => {} : props.onClick}
+      <span
         className={` ${
           props.disabled ? 'bg-gray-500' : 'bg-orange-500'
         } px-5 py-1.5 rounded-md`}
       >
         {props.children}
-      </button>
-    </div>
+      </span>
+    </button>
   );
 }
